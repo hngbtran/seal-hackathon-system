f4c0b1a070beb2711d1d4ce3397b56c0f2afd2f2
Ho Ngoc Bao Tran <hngbtran@gmail.com>
2026-06-09 14:33:23 +0700

    fix: correct user info on navbar in user dashboard

--- frontend/src/layouts/UserLayout.jsx
+++ frontend/src/layouts/UserLayout.jsx
@@ -1,15 +1,20 @@
 import Navbar from '../components/Navbar/Navbar'
 import styles from './UserLayout.module.css'
 
-const FAKE_USER = {
-  name: 'Nguyễn Thành Thái',
-  avatarUrl: null,
-}
+const userInfo=localStorage.getItem('userInfo')? JSON.parse(localStorage.getItem('userInfo')): null
 
 function UserLayout({ children, showCard = true }) {
     return (
         <div className={styles.page}>
-            <Navbar isLoggedIn={true} user={FAKE_USER} />
+            <Navbar 
+        isLoggedIn={true}
+        user={{
+          name:userInfo? userInfo.fullname : 'Nguyen Van A',
+          email: userInfo? userInfo.email : 'nguyenvana@example.com',
+          avatar: userInfo? userInfo.avatar : null
+        }}
+
+      />
             <main className={styles.main}>
                 {showCard
                     ? <div className={styles.card}>{children}</div>
